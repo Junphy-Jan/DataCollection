343ac0927f93a6d116cf17296375a30872acf407
Junphy <microjan@outlook.com>
2022-07-13 23:46:38 +0800

    保存路径修改

--- CommonUtils/constant.py
+++ CommonUtils/constant.py
@@ -6,4 +6,4 @@ WHEN_SAVE2DB = "23:59"
 COLLECT_INTERVAL = 30
 HOT_SEARCH_FILE_NAME = "hot_search_{}.json"
 HOME_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-WEIBO_SAVED_PATH = os.path.join(HOME_DIR, "collected_data/{}".format(HOT_SEARCH_FILE_NAME))
+WEIBO_SAVED_PATH = os.path.join(HOME_DIR, "collected_data/hot_search/{}".format(HOT_SEARCH_FILE_NAME))
